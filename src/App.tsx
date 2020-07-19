390624f46c0d4d1142b5a707a582192ca4acd80a
Daniel Jimenez <13410082+dannyoo@users.noreply.github.com>
2020-07-19 13:31:15 -0400

    init backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,81 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { db } from './firebase'
+import Container from '@material-ui/core/Container';
 
 function App() {
+
+  const [people, updatePeople] = useState<any>();
+  const [user, updateUser] = useState<any>();
+  const [collection, updateCollection] = useState<any>();
+  const [busy, updateBusy] = useState<any>();
+
+  function onUpdate(busy: boolean, collection: string, user: string) {
+    db.collection(collection).doc(user).update({
+      busy: busy
+    })
+      .then(function () {
+        console.log("Document successfully updated!");
+      })
+      .catch(function (error) {
+        // The document probably doesn't exist.
+        console.error("Error updating document: ", error);
+      });
+  }
+
+  useEffect(() => {
+
+    let location: string[] = window.location.pathname.split("/");
+    location.shift();
+    let [collection, user] = [...location];
+    updateUser(user);
+    updateCollection(collection);
+
+    let me = db.collection(collection).doc(user);
+
+    // Make sure User exists
+    me.get().then((docSnapshot) => {
+      if (!docSnapshot.exists) {
+        db.collection(collection).doc(user).set({
+          name: user,
+          busy: false
+        })
+          .then(function () {
+            console.log("Document successfully written!");
+            updateBusy(false);
+          })
+          .catch(function (error) {
+            console.error("Error writing document: ", error);
+          });
+      } else {
+        let it: boolean = docSnapshot?.data()?.busy;
+        updateBusy(it);
+      }
+    })
+
+    // Listen To Data
+    db.collection(collection)
+      .onSnapshot(function (querySnapshot) {
+        let people: string[] = [];
+        querySnapshot.forEach(function (doc: any) {
+          people.push(doc.data());
+        });
+        console.log("Current people in collection: ", people);
+        updatePeople(people);
+      });
+
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        user &&
+        <Container maxWidth="sm">
+          
+        </Container>
+      }
+
     </div>
   );
 }
---
+++ src/firebase.tsx
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+import 'firebase/firestore';
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyAB-jJ1vPnYCy0meyJYmjYNZSLJmro-7hI",
+    authDomain: "vibe-check-263f6.firebaseapp.com",
+    databaseURL: "https://vibe-check-263f6.firebaseio.com",
+    projectId: "vibe-check-263f6",
+    storageBucket: "vibe-check-263f6.appspot.com",
+    messagingSenderId: "975555934613",
+    appId: "1:975555934613:web:3f530706f9722c73339e9c"
+});
+
+const db = firebaseApp.firestore();
+
+export { db };
